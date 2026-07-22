5867535dc5fb206a61135d6aa7bfb78792036adb
udhya1233445 <udhayakumar07082007@gmail.com>
2026-07-22 10:37:26 +0530

    Create contains.java

---
+++ DAY -10/contains.java
@@ -0,0 +1,9 @@
+package day10;
+
+public class contains {
+    public static void main(String[] args){
+        String str="the quick brown fox";
+        System.out.println(str.contains("fox"));//(fox) string la element irrkanu verify pannum
+        System.out.println(str.contains("dog"));
+    }
+}
