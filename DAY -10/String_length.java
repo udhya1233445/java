5b6ade671b0d8e5a4c780d91c7fd8747dcfc5156
udhya1233445 <udhayakumar07082007@gmail.com>
2026-07-22 10:35:06 +0530

    Create String_length.java

---
+++ DAY -10/String_length.java
@@ -0,0 +1,9 @@
+package day10;
+
+public class String_length {
+    public static void main(String[] args){
+        String a="hello";
+        int len=a.length();
+        System.out.println(len);
+    }
+}
