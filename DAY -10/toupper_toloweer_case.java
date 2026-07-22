c19758ebb19600aa9473fb094bb61739839f3889
udhya1233445 <udhayakumar07082007@gmail.com>
2026-07-22 10:40:21 +0530

    Create toupper_toloweer_case.java

---
+++ DAY -10/toupper_toloweer_case.java
@@ -0,0 +1,10 @@
+package day10;
+
+public class toupper_toloweer_case {
+    public static void main(String[] args){
+        String str="Hello World";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+    }
+}
