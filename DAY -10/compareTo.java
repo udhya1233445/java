e0c555a2075281659562e49989668f4c8ba30ccb
udhya1233445 <udhayakumar07082007@gmail.com>
2026-07-22 10:36:48 +0530

    Create compareTo.java

---
+++ DAY -10/compareTo.java
@@ -0,0 +1,9 @@
+package day10;
+
+public class compareTo {
+    public static void main(String[] args){
+        String name=new String("hello");
+        String name1=new String("hello");
+        System.out.println(name.compareTo(name1));
+    }
+}
