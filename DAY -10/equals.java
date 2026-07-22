5c8c2a5d1eb8824f0eb5d7bb2c1639966696d424
udhya1233445 <udhayakumar07082007@gmail.com>
2026-07-22 10:37:57 +0530

    Create equals.java

---
+++ DAY -10/equals.java
@@ -0,0 +1,11 @@
+package day10;
+
+public class equals {
+    public static void main(String[] args){
+        String str1="jackfruit";
+        String str2="banana";
+        String str3="jackfruit";
+        System.out.println(str1.equals(str3));
+        System.out.println(str1.equals(str2));
+    }
+}
