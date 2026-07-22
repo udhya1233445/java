dcdad67a0145f357992b9f8485d60a27df03ba14
udhya1233445 <udhayakumar07082007@gmail.com>
2026-07-22 10:39:31 +0530

    Create replace.java

---
+++ DAY -10/replace.java
@@ -0,0 +1,9 @@
+package day10;
+
+public class replace {
+    public static void main(String[] args){
+        String name="hello";
+        String name1="hello";
+        System.out.println(name.replace("h","j"));// another replaceall will get all the word
+    }
+}
